187fe7938f61e6362936dd3d0bef7467f077c1c4
Faadhil Ahamat <faadhil.aa@gmail.com>
2021-08-17 20:58:23 +0000

    implemented assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length){
+    return false;
+  } else {
+    for (let x = 0; x < array1.length; x++){
+      if (array1[x] !== array2[x]){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2){
+  let comparison = eqArrays(array1,array2);
+  console.log(comparison);
+  if (comparison === true){
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  }
+
+}
+
