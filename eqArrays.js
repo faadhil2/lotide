12ac118004349917f05ffa1d752742130f84d212
Faadhil Ahamat <faadhil.aa@gmail.com>
2021-08-17 20:45:30 +0000

    created eqArrays.js function

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length){
+    return false;
+  } else {
+    for (let x = 0; x < array1.length; x++){
+      if (array1[x] !== array2[x]){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+//TEST CASES
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 2]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
